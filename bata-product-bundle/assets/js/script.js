b954d7f34772ae8b4a588eaa88fdafe75d5ef65d
RafiaBonna <rafiaakterbonna@gmail.com>
2025-09-15 12:18:14 +0600

    my

--- bata-product-bundle/assets/js/script.js
+++
@@ -1,9 +0,0 @@
-jQuery(document).ready(function($) {
-    $('.add-to-cart-bundle-btn').on('click', function(e) {
-        e.preventDefault();
-        
-        // This is a placeholder. You will add AJAX code here to add all products
-        // from the bundle to the cart and apply the discount.
-        alert('Product bundle added to cart!');
-    });
-});
